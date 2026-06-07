229d493ca648b68ffc24170a81daf6d6e08c8897
Michael Agbozo <agbozomykell8@gmail.com>
2026-06-07 22:43:21 +0000

    Add a temporary search to locate fastcgi_params on the deployment server

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ chmod -R 777 /app/storage /app/bootstrap/cache
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
+find / -name "fastcgi_params" 2>/dev/null
 php-fpm -y /assets/php-fpm.conf -D
 nginx -c /app/nginx.conf
