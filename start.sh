75491dc0401c0ecb2047c54eda35f44603267ce8
Michael Agbozo <agbozomykell8@gmail.com>
2026-06-07 20:18:51 +0000

    Add start.sh script for production server startup (migrations, caching, nginx)

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p /var/log/nginx
+chmod -R 777 /app/storage /app/bootstrap/cache
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
+nginx -c /nginx.conf
