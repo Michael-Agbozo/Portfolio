4f64bf19a805ec0c54653edaa3a3cf000881baec
Michael Agbozo <agbozomykell8@gmail.com>
2026-06-07 23:40:06 +0000

    Revert to Nixpacks' own prestart.mjs for nginx config generation, remove custom nginx.conf

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ chmod -R 777 /app/storage /app/bootstrap/cache
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
-php-fpm -y /assets/php-fpm.conf -D
-nginx -c /app/nginx.conf
+node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
+nginx -c /nginx.conf
