4c180b311940a8bfab1e0e24773ad5473a915845
Michael Agbozo <agbozomykell8@gmail.com>
2026-06-07 23:53:23 +0000

    Start php-fpm before nginx so PHP requests can be served

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ chmod -R 777 /app/storage /app/bootstrap/cache
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
+php-fpm -D
 node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
 nginx -c /nginx.conf
