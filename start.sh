d206fb19ea3a3dcad81b6a50f822f527788a0cb3
Michael Agbozo <agbozomykell8@gmail.com>
2026-06-08 12:41:36 +0000

    fix: add storage:link to startup script

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ chmod -R 777 /app/storage /app/bootstrap/cache
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
+php artisan storage:link --force 2>/dev/null || true
 php-fpm -y /assets/php-fpm.conf -D
 node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
 
